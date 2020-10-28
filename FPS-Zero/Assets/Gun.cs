ba9ddc7471b6f57e2c814f69f246905147556e93
Zeshan Rasul <zeshanrasul@gmail.com>
2020-10-28 18:45:30 +0000

    Shooting and Enemy logic

---
+++ FPS-Zero/Assets/Gun.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+    public float damage = 10f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
+
+    public GameObject impactEffect;
+
+    float nextTimeToFire = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + (1f / fireRate);
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                hit.collider.GetComponent<Enemy>().TakeDamage(damage);
+                
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject hitParticleEffect = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(hitParticleEffect, 2f);
+
+        }
+
+       
+    }
+}
